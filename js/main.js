b15fd59d2f5b399cf3e1451f9d4dce3cce3badab
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-07-06 09:30:47 +0200

    add click event and handleInput function with conditionals

--- js/main.js
+++ js/main.js
@@ -13,4 +13,51 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log(getRandomNumber(100));
+//Comprobación que funciona
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+/*Construir función handleInput. Al hacer click:
+IMPORTANTE: lo que escriba la usuaria es un string, pasarlo a número con parseInt();
+1-Comprobar si el número dado por la usuaria es = al random. Si es = gana, si no da pistas. Usar condicionales.
+
+*/
+
+//Función para conseguir el número que pinta la usuaria
+// function getUserNumber() {
+//   const getNumber = putNumber.value;
+//   const transformToNumber = parseInt(getNumber);
+//   console.log(transformToNumber);
+//   console.log(typeof transformToNumber);
+
+//   const randomNumber = getRandomNumber(100);
+
+//   if (transformToNumber === randomNumber) {
+//     console.log("Has ganado campeona!!!");
+//   }
+// }
+
+// function conditional (){
+
+// }
+
+function handleInput() {
+  const getNumber = putNumber.value;
+  const transformToNumber = parseInt(getNumber);
+  console.log(transformToNumber);
+  console.log(typeof transformToNumber);
+
+  //const randomNumber = getRandomNumber(100);
+
+  if (transformToNumber === randomNumber) {
+    console.log("Has ganado campeona!!!");
+  } else if (transformToNumber < randomNumber) {
+    console.log("Demasiado bajo");
+  } else if (transformToNumber > randomNumber) {
+    console.log("Demasiado alto");
+  }
+}
+
+//Añadir el evento click
+
+tryBtn.addEventListener("click", handleInput);
